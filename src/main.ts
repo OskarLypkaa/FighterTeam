eb64cc91d814bc045ce5c429e6d6ed68abaef6de
Oskar Łypka <oskar.lypka@ige-xao.com>
2025-08-20 20:07:12 +0200

    Yet another test

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 // src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
     providers: [
         provideHttpClient(withInterceptorsFromDi()),
-        provideNoopAnimations(), // <-- zero zależności na @angular/animations
     ],
 }).catch(err => console.error(err));
