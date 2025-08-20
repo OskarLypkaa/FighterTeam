684c7f79866c5bc97c44dc4b62a5acc49287de20
Oskar Łypka <oskar.lypka@ige-xao.com>
2025-08-20 19:51:11 +0200

    Adding new animation style

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 // src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
     providers: [
-        provideHttpClient(withInterceptorsFromDi()), // HttpClient dla serwisów
-        provideAnimationsAsync()                     // wymagane przez Angular Material
-    ]
+        provideHttpClient(withInterceptorsFromDi()),
+        provideNoopAnimations(), // <-- zero zależności na @angular/animations
+    ],
 }).catch(err => console.error(err));
